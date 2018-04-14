3faea20714d62724facc2c189feb147ac1199595
ChengFeng AO <414356022@qq.com>
2018-04-14 19:11:00 +0800

    fizzBuzzGame and test

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -1,5 +1,7 @@
 package com.thoughtworks;
 
+import org.omg.Dynamic.Parameter;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,13 +9,71 @@ import java.util.List;
 public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
-    public void start(int count) {
-
+    public List<String> start(int count) {   //传入初始值
+        for (int i = 1; i <=count; i++) {
+        if(!fizz_when_have_3(i).equals("-1"))
+        {
+            results.add(fizz_when_have_3(i));
+        }
+        else if(!when_multiplier_of_35_37_357(i).equals("-1"))
+        {
+            results.add(when_multiplier_of_35_37_357(i));
+        } else if (!when_multiplier_of_3_5_7(i).equals("-1")) {
+            results.add(when_multiplier_of_3_5_7(i));
+        }
+        else {
+            results.add(i+"");
+        }
+      }
+      return results;
     }
 
-    public List<String> getResults() {
+    public  String fizz_when_have_3(int number) {   //包含3的
+        String result="-1";
+        if((number+"").contains("3"))
+        {
+            result ="Fizz";
+        }
+            return result;
+    }
+
+    public String when_multiplier_of_35_37_357(int number) {
+        String result="-1";
+      if(number%3==0&&number%5==0&&number%7==0)
+      {
+          result="FizzBuzzWhizz";
+      }
+      else if (number % 3 == 0 && number % 5 == 0) {
+          result="FizzBuzz";
+      } else if (number%5==0&&number%7==0) {
+          result="BuzzWhizz";
+      } else if (number%3==0&&number%7==0) {
+          result="FizzWhizz";
+      }
+      return result;
+    }
+
+    public String when_multiplier_of_3_5_7(int number) {
+         String result="-1";
+        if(number%3==0)
+        {
+            result="Fizz";
+        }
+        if (number%5==0)
+        {
+            result="Buzz";
+        }
+        if (number%7==0)
+        {
+            result="Whizz";
+        }
+        return result;
+    }
+
+    public List<String> getResults() { //返回结果
         return results;
     }
+
 }
 
 
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,46 @@
 package com.thoughtworks;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class FizzBuzzGameTest {
+    private FizzBuzzGame fizzBuzzGameTest;
+
+    @Before
+    public void setup() {
+        fizzBuzzGameTest=mock(FizzBuzzGame.class);
+    }
     @Test
-    public void should_return_Fizz_when_have_3() {
+    public void test_Fizz_when_have_3() {
+     when(fizzBuzzGameTest.fizz_when_have_3(13)).thenReturn("Fizz");
+     when(fizzBuzzGameTest.fizz_when_have_3(12)).thenReturn("-1");
+    }
+
+
+    @Test
+    public void test_when_multiplier_of_35_37_357() {
+        when(fizzBuzzGameTest.when_multiplier_of_35_37_357(15)).thenReturn("FizzBuzz");
+        when(fizzBuzzGameTest.when_multiplier_of_35_37_357(21)).thenReturn("FizzWhizz");
+        when(fizzBuzzGameTest.when_multiplier_of_35_37_357(35)).thenReturn("BuzzWhizz");
+        when(fizzBuzzGameTest.when_multiplier_of_35_37_357(1)).thenReturn("-1");
 
     }
+    @Test
+    public void test_when_multiplier_of_3_5_7()
+    {
+        when(fizzBuzzGameTest.when_multiplier_of_3_5_7(39)).thenReturn("Fizz");
+        when(fizzBuzzGameTest.when_multiplier_of_3_5_7(25)).thenReturn("Buzz");
+        when(fizzBuzzGameTest.when_multiplier_of_3_5_7(35)).thenReturn("Whizz");
+    }
+
 }
